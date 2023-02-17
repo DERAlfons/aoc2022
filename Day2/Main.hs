357906f5e6a51b39a48c03dd57fb0a23c7afff51
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-02-17 20:30:03 +0100

    Refactor Day2 code

--- Day2/Main.hs
+++ Day2/Main.hs
@@ -1,56 +1,56 @@
 module Day2.Main (main) where
 
-data RPS = Rock | Paper | Scissors deriving Eq
+import Data.Char (ord)
 
-parse :: String -> RPS
-parse "A" = Rock
-parse "B" = Paper
-parse "C" = Scissors
-parse "X" = Rock
-parse "Y" = Paper
-parse "Z" = Scissors
+data RPS = Rock | Paper | Scissors deriving (Eq, Enum)
+
+toRPS :: Char -> RPS
+toRPS c = toEnum $ (ord c - ord 'A') `mod` (ord 'X' - ord 'A')
+
+toAction :: Char -> (RPS -> RPS)
+toAction 'X' = lose
+toAction 'Y' = draw
+toAction 'Z' = win
+
+parseMatch1 :: [Char] -> (RPS, RPS)
+parseMatch1 [c1, c2] = (toRPS c1, toRPS c2)
+
+parseMatch2 :: [Char] -> (RPS, RPS -> RPS)
+parseMatch2 [c1, c2] = (toRPS c1, toAction c2)
 
 rpsPoints :: RPS -> Int
-rpsPoints Rock = 1
-rpsPoints Paper = 2
-rpsPoints Scissors = 3
+rpsPoints rps = fromEnum rps + 1
 
 matchPoints :: RPS -> RPS -> Int
-matchPoints Rock Paper = 6
-matchPoints Paper Scissors = 6
-matchPoints Scissors Rock = 6
-matchPoints a b
-    | a == b = 3
-matchPoints _ _ = 0
+matchPoints rps1 rps2
+    | rps1 == win  rps2 = 6
+    | rps1 == draw rps2 = 3
+    | rps1 == lose rps2 = 0
+
+points :: RPS -> RPS -> Int
+points rps1 rps2 = rpsPoints rps1 + matchPoints rps1 rps2
 
 win :: RPS -> RPS
-win Rock = Paper
-win Paper = Scissors
-win scissors = Rock
+win rps = toEnum $ (fromEnum rps + 1) `mod` 3
 
 draw :: RPS -> RPS
 draw = id
 
 lose :: RPS -> RPS
-lose Rock = Scissors
-lose Paper = Rock
-lose Scissors = Paper
-
-points1 :: [String] -> Int
-points1 [a, b] = rpsPoints (parse b) + matchPoints (parse a) (parse b)
-
-points2 :: [String] -> Int
-points2 [a, "X"] = 0 + (rpsPoints $ lose $ parse a)
-points2 [a, "Y"] = 3 + (rpsPoints $ draw $ parse a)
-points2 [a, "Z"] = 6 + (rpsPoints $ win $ parse a)
+lose rps = toEnum $ (fromEnum rps - 1) `mod` 3
 
 main :: IO (String, String)
 main = do
-    strat <- map words <$> lines <$> readFile "Day2/input.txt"
-    let score1 = sum $ map points1 strat
+    instructions <- map (map head . words) . lines <$> readFile "Day2/input.txt"
+
+    let strategy1 = map parseMatch1 instructions
+        score1 = sum $ map (\(rps1, rps2) -> points rps2 rps1) strategy1
     putStr "Total score with code 1: "
     print score1
-    let score2 = sum $ map points2 strat
+
+    let strategy2 = map parseMatch2 instructions
+        score2 = sum $ map (\(rps, act) -> points (act rps) rps) strategy2
     putStr "Total score with code 2: "
     print score2
+
     return (show score1, show score2)
