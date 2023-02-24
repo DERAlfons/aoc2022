29bb61915936022b02533bad0851567e4cd28fac
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-02-24 15:22:28 +0100

    Day9 code cosmetics

--- Day9/Main.hs
+++ Day9/Main.hs
@@ -1,23 +1,25 @@
 module Day9.Main (main) where
 
-import Data.List
+import Data.List (scanl', nub)
 
 import My.Util (explode)
 
-step :: Char -> (Int, Int) -> (Int, Int)
+type Point = (Int, Int)
+
+step :: Char -> Point -> Point
 step 'U' (x, y) = (x, y + 1)
 step 'D' (x, y) = (x, y - 1)
 step 'L' (x, y) = (x - 1, y)
 step 'R' (x, y) = (x + 1, y)
 
-follow :: (Int, Int) -> (Int, Int) -> (Int, Int)
+follow :: Point -> Point -> Point
 follow (xH, yH) (xT, yT) = case (abs (xH - xT), abs (yH - yT)) of
     (2, 2) -> ((xH + xT) `div` 2, (yH + yT) `div` 2)
     (2, _) -> ((xH + xT) `div` 2, yH)
     (_, 2) -> (xH, (yH + yT) `div` 2)
     (_, _) -> (xT, yT)
 
-update :: [(Int, Int)] -> Char -> [(Int, Int)]
+update :: [Point] -> Char -> [Point]
 update (pH : pTs) direction = scanl' follow (step direction pH) pTs
 
 parseInstruction :: String -> [Char]
@@ -27,13 +29,16 @@ parseInstruction s =
 
 main :: IO (String, String)
 main = do
-    instructions <- concat . map parseInstruction . lines <$> readFile "Day9/input.txt"
-    let htPositions2 = scanl' update [(0, 0), (0, 0)] instructions
-        tPosCount2 = length $ nub $ map last htPositions2
-    putStrLn "Number of positions visited by end of rope of length 2:"
-    putStrLn $ show tPosCount2
-    let htPositions10 = scanl' update (replicate 10 (0, 0)) instructions
-        tPosCount10 = length $ nub $ map last htPositions10
-    putStrLn "Number of positions visited by end of rope of length 10:"
-    putStrLn $ show tPosCount10
-    return (show tPosCount2, show tPosCount10)
+    instructions <- (parseInstruction =<<) . lines <$> readFile "Day9/input.txt"
+
+    let ropePositions2 = scanl' update (replicate 2 (0, 0)) instructions
+        lastKnotPosCount2 = length $ nub $ map last ropePositions2
+    putStr "Number of positions visited by end of rope of length 2: "
+    print lastKnotPosCount2
+
+    let ropePositions10 = scanl' update (replicate 10 (0, 0)) instructions
+        lastKnotPosCount10 = length $ nub $ map last ropePositions10
+    putStr "Number of positions visited by end of rope of length 10: "
+    print lastKnotPosCount10
+
+    return (show lastKnotPosCount2, show lastKnotPosCount10)
