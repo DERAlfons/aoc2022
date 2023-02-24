501af0026f54fe61b7262f9b0e260b4df77becff
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-02-24 22:48:26 +0100

    Day10 code refactor

--- Day10/Main.hs
+++ Day10/Main.hs
@@ -1,16 +1,11 @@
 module Day10.Main (main) where
 
-import Data.List
-import Data.Maybe (fromJust)
+import Data.List (scanl')
+import Data.Maybe (maybeToList)
 import Data.Foldable (asum)
 
+import My.Util (groupsOf)
 import My.Parser (parserRegex, run)
-
-groupsOf :: Int -> [a] -> [[a]]
-groupsOf _ [] = []
-groupsOf n bs =
-    let (g, rest) = splitAt n bs
-    in g : groupsOf n rest
 
 parse :: String -> Maybe [Int -> Int]
 parse = run $ asum [
@@ -19,15 +14,20 @@ parse = run $ asum [
 
 main :: IO (String, String)
 main = do
-    instructions <- concat . map (fromJust . parse) . lines <$> readFile "Day10/input.txt"
+    instructions <- (concat . maybeToList . parse =<<) . lines <$>
+        readFile "Day10/input.txt"
     let xReg = scanl' (flip ($)) 1 instructions
-        signal = zipWith (*) [1 ..] xReg
+
+    let signal = zipWith (*) [1 ..] xReg
         result = sum $ map (signal !!) [19, 59 .. 219]
-    putStrLn "Sum of signals:"
-    putStrLn $ show result
+    putStr "Sum of signals: "
+    print result
+
+    let displayLines = do
+            xLine <- groupsOf 40 xReg
+            let drawLine = zipWith (\pos x -> abs (x - pos) <= 1) [0 ..] xLine
+            return $ map (\p -> if p then '#' else '.') drawLine
     putStrLn "Display output:"
-    sequence $ do
-        xLine <- groupsOf 40 xReg
-        let drawLine = zipWith (\pos x -> abs (x - pos) <= 1) [0 ..] xLine
-        return $ putStrLn $ map (\p -> if p then '#' else '.') drawLine
-    return (show result, "")
+    sequence $ map putStrLn displayLines
+
+    return (show result, concat displayLines)
