32479eb26d8bc08b83dd9a428f816b6a37b3ad24
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-02-27 02:15:21 +0100

    Day12 code refactor

--- Day12/Main.hs
+++ Day12/Main.hs
@@ -1,36 +1,43 @@
 module Day12.Main (main) where
 
-import Data.List
-import Data.Map (Map, fromList, (!))
-import Data.Char (ord)
-import Control.Monad
+import Prelude hiding (lookup, filter)
+import Data.Map (Map, fromList, mapWithKey, insert, lookup, (!), filter, keys)
+import Data.Maybe (maybeToList)
+import Control.Monad (guard)
 
 import Algorithm.Search (dijkstraAssoc)
 
-graph :: (Int -> Int -> Bool) -> [[Int]] -> Map (Int, Int) [((Int, Int), Int)]
-graph condition hMap = fromList $ do
-    (i, row) <- zip [0 ..] hMap
-    (j, h) <- zip [0 ..] row
-    return ((i, j), do
-        (iN, jN) <- [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
-        guard $ (iN >= 0) && (jN >= 0) && (iN < length hMap) && (jN < length row)
-        guard $ condition h (hMap !! iN !! jN)
-        return ((iN, jN), 1))
+import My.Util (maybeToIO)
+
+type Point = (Int, Int)
+
+graph :: (Char -> Char -> Bool) -> Map Point Char -> Map Point [(Point, Int)]
+graph condition hMap = flip mapWithKey hMap $ \(i, j) h -> do
+    pN <- [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+    hN <- maybeToList $ lookup pN hMap
+    guard $ condition h hN
+    return (pN, 1)
 
 main :: IO (String, String)
 main = do
-    seMap <- lines <$> readFile "Day12/input.txt"
-    let Just jS = msum $ map (elemIndex 'S') seMap
-        Just iS = msum $ map (elemIndex 'S') $ transpose seMap
-        Just jE = msum $ map (elemIndex 'E') seMap
-        Just iE = msum $ map (elemIndex 'E') $ transpose seMap
-        hMap = [[case e of {'S' -> ord 'a'; 'E' -> ord 'z'; x -> ord x} | e <- row] | row <- seMap]
-        graphM = graph (\h n -> n <= h + 1) hMap
-        Just (cost, _) = dijkstraAssoc (graphM !) (== (iE, jE)) (iS, jS)
-    putStrLn "Shortest path from start to top:"
-    putStrLn $ show cost
-    let graphMi = graph (\h n -> h <= n + 1) hMap
-        Just (costI, _) = dijkstraAssoc (graphMi !) (\(i, j) -> hMap !! i !! j == ord 'a') (iE, jE)
-    putStrLn "Shortest path from any lowest point to top:"
-    putStrLn $ show costI
-    return (show cost, show costI)
+    input <- lines <$> readFile "Day12/input.txt"
+    let seMap = fromList [((i, j), h) |
+            (i, row) <- zip [0 ..] input,
+            (j, h) <- zip [0 ..] row]
+        start = head $ keys $ filter (== 'S') seMap
+        end = head $ keys $ filter (== 'E') seMap
+        hMap = insert start 'a' $ insert end 'z' seMap
+
+    let graphUp = graph (\h hN -> hN <= succ h) hMap
+    (pathLength1, _) <- maybeToIO "no path found" $
+        dijkstraAssoc (graphUp !) (== end) start
+    putStr "Shortest path from start to top: "
+    print pathLength1
+
+    let graphDown = graph (\h hN -> h <= succ hN) hMap
+    (pathLength2, _) <- maybeToIO "no path found" $
+        dijkstraAssoc (graphDown !) (\p -> hMap ! p == 'a') end
+    putStr "Shortest path from any lowest point to top: "
+    print pathLength2
+
+    return (show pathLength1, show pathLength2)
