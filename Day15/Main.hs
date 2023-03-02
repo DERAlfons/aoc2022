73f7f24248facee12064f758ba4571dfcafcc9f1
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-03-02 18:22:21 +0100

    Day15 code refactor

--- Day15/Main.hs
+++ Day15/Main.hs
@@ -1,51 +1,72 @@
 module Day15.Main (main) where
 
-import Data.List
-import Data.Maybe
-import Control.Monad
+import Data.List (sort)
+import Data.Maybe (maybeToList, listToMaybe)
+import Control.Monad (guard)
 
-import My.Parser
+import My.Util (maybeToIO)
+import My.Parser (parserRegex, run)
 
-slice :: Int -> (Int, Int, Int, Int) -> Maybe (Int, Int)
-slice line (sx, sy, bx, by) =
-    let distB = abs (sx - bx) + abs (sy - by)
-        distL = abs (line - sy)
-        dist = distB - distL
-    in do
-        guard $ dist >= 0
-        return (sx - dist, sx + dist)
+type Point = (Int, Int)
+type Sensor = (Point, Int)
+type Range = (Int, Int)
 
-reduce :: [(Int, Int)] -> [(Int, Int)]
+distance :: Point -> Point -> Int
+distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+hSlice :: Int -> Sensor -> Maybe Range
+hSlice y ((sx, sy), radius) = do
+    let vDist = abs (y - sy)
+        hDist = radius - vDist
+    guard $ hDist >= 0
+    return (sx - hDist, sx + hDist)
+
+reduce :: [Range] -> [Range]
 reduce [] = []
 reduce [s] = [s]
-reduce ((s1, e1) : (s2, e2) : rest) =
-    if s2 <= e1 then
-        reduce $ (s1, max e1 e2) : rest
-    else
-        (s1, e1) : reduce ((s2, e2) : rest)
+reduce ((s1, e1) : (s2, e2) : rest)
+    | s2 <= e1 = reduce $ (s1, max e1 e2) : rest
+    | otherwise = (s1, e1) : reduce ((s2, e2) : rest)
 
-count :: [(Int, Int)] -> Int
-count = sum . map (\(s, e) -> e - s)
+count :: [Range] -> Int
+count = sum . map (\(s, e) -> e - s) . reduce . sort
 
-parseSB :: Parser (Int, Int, Int, Int)
-parseSB = parserRegex "Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)" (\[sx, sy, bx, by] -> (read sx, read sy, read bx, read by))
+unscanned :: [Sensor] -> Range -> Range -> Maybe Point
+unscanned sensors (minX, maxX) (minY, maxY) = listToMaybe $ do
+    y <- [minY .. maxY]
+    let yLine = maybeToList . hSlice y =<< sensors
+        r = reduce $ sort yLine
+        k = dropWhile ((< minX) . snd) r
+    case k of
+        [] -> return (minX, y)
+        (s, e) : _
+            | s > minX -> return (minX, y)
+            | e < maxX -> return (e + 1, y)
+        otherwise -> []
 
-parse :: String -> (Int, Int, Int, Int)
-parse = fromJust . run parseSB
+parseSensor :: String -> Maybe Sensor
+parseSensor = run $ parserRegex
+    "Sensor at x=(-?\\d+), y=(-?\\d+): \
+        \closest beacon is at x=(-?\\d+), y=(-?\\d+)" $
+    \[sx, sy, bx, by] ->
+        let sensorOrigin = (read sx, read sy)
+            beacon = (read bx, read by) in
+        (sensorOrigin, distance sensorOrigin beacon)
 
 main :: IO (String, String)
 main = do
-    sbs <- map parse . lines <$> readFile "Day15/input.txt"
-    let line = sbs >>= maybeToList . slice 2000000
-        result1 = count $ reduce $ sort line
-    putStrLn "Scanned positions in row 2000000:"
-    putStrLn $ show result1
-    let (bx, by) = head $ do
-            y <- [0 .. 4000000]
-            let lineY = sbs >>= maybeToList . slice y
-                r = reduce $ sort lineY
-            guard $ length r == 2
-            return $ ((+ 1) $ snd $ head r, y)
-    putStrLn "Position of missing beacon:"
-    putStrLn $ show $ 4000000 * bx + by
-    return (show result1, show $ 4000000 * bx + by)
+    sensors <- (maybeToList . parseSensor =<<) <$>
+        lines <$> readFile "Day15/input.txt"
+
+    let scanLine = maybeToList . hSlice 2000000 =<< sensors
+        scanCount = count scanLine
+    putStr "Scanned positions in row 2000000: "
+    print scanCount
+
+    (bx, by) <- maybeToIO "error: all positions are scanned" $
+        unscanned sensors (0, 4000000) (0, 4000000)
+    let tuningFrequency = 4000000 * bx + by
+    putStr "Position of missing beacon: "
+    print tuningFrequency
+
+    return (show scanCount, show tuningFrequency)
