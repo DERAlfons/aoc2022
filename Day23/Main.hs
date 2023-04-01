a40c3ac373d4a4aa2e853f142f71b7dfb04ffa8b
Jascha Bakarinow <jascha-bakarinow@web.de>
2023-04-01 22:06:40 +0200

    Day23 code refactor

--- Day23/Main.hs
+++ Day23/Main.hs
@@ -1,71 +1,66 @@
 module Day23.Main (main) where
 
-import Data.Set (Set, fromList, notMember)
+import Data.Set (Set, size, notMember)
 import qualified Data.Set as S
 import Data.Map (Map, fromListWith)
 import qualified Data.Map as M
-import Control.Monad
+import Control.Monad (guard)
 
-run :: Set (Int, Int) -> Int
-run elves = loop 1 0 elves
-    where
-    loop n rot elves =
-        let (newElves, finished) = step elves rot
-        in if finished then
-               n
-           else
-               loop (n + 1) ((rot + 1) `mod` 4) newElves
+import My.Util (applyN)
 
-update :: Int -> Set (Int, Int) -> Set (Int, Int)
-update n elves = loop n 0 elves
-    where
-    loop 0 _ elves = elves
-    loop n rot elves = loop (n - 1) ((rot + 1) `mod` 4) (fst $ step elves rot)
+type Point = (Int, Int)
 
-step :: Set (Int, Int) -> Int -> (Set (Int, Int), Bool)
-step elves rot =
-    let props = M.toList $ fromListWith (++) $ do
-            e <- S.toList elves
-            return (getProp elves rot e, [e])
-        newElves = fromList $ do
-            (p, es) <- props
-            case es of
-                [_] -> return p
-                otherwise -> es
-        finished = all (\(p, es) -> es == [p]) props
-    in (newElves, finished)
+run :: (Set Point, Int) -> (Set Point, Int)
+run (elves, round) = let
+    (newElves, newRound) = step (elves, round) in
+    if newElves == elves then
+        (newElves, newRound)
+    else
+        run (newElves, newRound)
 
-getProp :: Set (Int, Int) -> Int -> (Int, Int) -> (Int, Int)
-getProp elves rot (ex, ey) =
-    let props = [
-            if all (flip notMember elves) [(ex - 1, ey - 1), (ex, ey - 1), (ex + 1, ey - 1)] then [(ex, ey - 1)] else [],
-            if all (flip notMember elves) [(ex - 1, ey + 1), (ex, ey + 1), (ex + 1, ey + 1)] then [(ex, ey + 1)] else [],
-            if all (flip notMember elves) [(ex - 1, ey - 1), (ex - 1, ey), (ex - 1, ey + 1)] then [(ex - 1, ey)] else [],
-            if all (flip notMember elves) [(ex + 1, ey - 1), (ex + 1, ey), (ex + 1, ey + 1)] then [(ex + 1, ey)] else []]
-        rProps = concat $ (drop rot props) ++ (take rot props)
-    in case length rProps of
-        0 -> (ex, ey)
-        4 -> (ex, ey)
-        otherwise -> head rProps
+step :: (Set Point, Int) -> (Set Point, Int)
+step (elves, round) = let
+    props = fromListWith (++) $ do
+        e <- S.toList elves
+        return (getProp elves (round `mod` 4) e, [e])
+    newElves = S.fromList $ do
+        (p, es) <- M.toList props
+        if length es == 1 then return p else es in
+    (newElves, round + 1)
+
+getProp :: Set Point -> Int -> Point -> Point
+getProp elves rot (x, y) = let
+    props = take 4 $ drop rot $ cycle [
+        [(x, y - 1), (x - 1, y - 1), (x + 1, y - 1)],
+        [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)],
+        [(x - 1, y), (x - 1, y - 1), (x - 1, y + 1)],
+        [(x + 1, y), (x + 1, y - 1), (x + 1, y + 1)]]
+    cProps = map head $ filter (all (`notMember` elves)) props in
+    case length cProps of
+        0 -> (x, y)
+        4 -> (x, y)
+        otherwise -> head cProps
 
 main :: IO (String, String)
 main = do
     m <- lines <$> readFile "Day23/input.txt"
-    let elves = fromList $ do
-            (i, line) <- zip [0, 1 ..] m
-            do
-                (j, pos) <- zip [0, 1 ..] line
-                guard $ pos == '#'
-                return (j, i)
-        cList = S.toList $ update 10 elves
-        minX = minimum $ map fst cList
-        maxX = maximum $ map fst cList
-        minY = minimum $ map snd cList
-        maxY = maximum $ map snd cList
-        result = (maxX - minX + 1) * (maxY - minY + 1) - length cList
-    putStrLn "Empty ground after 10 rounds:"
-    putStrLn $ show result
-    let rounds = run elves
-    putStrLn "Total number of rounds:"
-    putStrLn $ show rounds
-    return (show result, show rounds)
+    let elves = S.fromList $ do
+            (i, line) <- zip [0 ..] m
+            (j, pos) <- zip [0 ..] line
+            guard $ pos == '#'
+            return (j, i)
+
+    let elves10 = fst $ applyN 10 step (elves, 0)
+        minX = minimum $ S.map fst elves10
+        maxX = maximum $ S.map fst elves10
+        minY = minimum $ S.map snd elves10
+        maxY = maximum $ S.map snd elves10
+        emptyGround = (maxX - minX + 1) * (maxY - minY + 1) - size elves10
+    putStr "Empty ground after 10 rounds: "
+    print emptyGround
+
+    let rounds = snd $ run (elves, 0)
+    putStr "Total number of rounds: "
+    print rounds
+
+    return (show emptyGround, show rounds)
